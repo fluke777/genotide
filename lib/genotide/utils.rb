68acf97adece40c9fafafd6683e9c5a298ce811d
Tomas Svarovsky <svarovsky@gooddata.com>
2013-05-13 23:54:34 -0700

    Commit

---
+++ lib/genotide/utils.rb
@@ -0,0 +1,27 @@
+module Genotide
+  
+  module Utils
+
+    def generate_gooddata_day_lookup
+
+    # 
+    # date = Date.new(1900, 1, 1)
+    # 
+    # map = {}
+    # 
+    # (200 * 365).times do
+    #   timestamp = Time.utc(date.year, date.month, date.day).to_i
+    #   gdc_date_id = timestamp/86400 + 25568
+    #   map[date.to_s] = gdc_date_id
+    #   date = date.next
+    # end
+    # 
+    # File.open('gdc_lookup.json', 'w') do |f|
+    #   f.write JSON.pretty_generate(map)
+    # end
+
+    end
+
+  end
+  
+end
